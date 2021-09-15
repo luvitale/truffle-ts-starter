1fa5337bec59e0c11ef9e50b6920438e8963eb96
Mian Muhammad Sharjeel Safdar <miansharjeelsafdar@gmail.com>
2021-09-15 16:26:41 +0500

    Added contract verification feature, updated tsconfig

--- README.md
+++ README.md
@@ -18,3 +18,19 @@ yarn migrate
 ## Known limitations
 
 Migrations need to be transpiled to js before execution. Use `yarn migrate` instead of `truffle migrate` to transpile and migrate with a single command.
+
+## Command for Verifying Smart Contract Source Code
+
+After deployment, run the following command with one or more contracts that you wish to verify:
+
+```sh
+truffle run verify SomeContractName AnotherContractName --network networkName
+```
+
+You can optionally provide an explicit address of the contract(s) that you wish to verify. This may be useful when you have deployed multiple instances of the same contract. The address is appended with @<address> as follows:
+
+```sh
+truffle run verify ContractName@0x61C9157A9EfCaf6022243fA65Ef4666ECc9FD3D7 --network rinkeby
+```
+
+More details can be found [here](https://github.com/rkalis/truffle-plugin-verify).
--- truffle-config.js
+++ truffle-config.js
@@ -1,30 +1,81 @@
 require("dotenv").config({ path: "./.env" });
 const HDWalletProvider = require("@truffle/hdwallet-provider");
-const mnemonic = process.env.MNEMONIC;
+const MNEMONIC = process.env.MNEMONIC;
 const INFURA_PROJECT_ID = process.env.INFURA_PROJECT_ID;
-const RPC_ENDPOINT = `https://ropsten.infura.io/v3/${INFURA_PROJECT_ID}`;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 module.exports = {
-  networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*",
-    },
-    ropsten: {
-      provider: () => new HDWalletProvider(mnemonic, RPC_ENDPOINT),
-      network_id: 3, // Ropsten's id
-      gas: 5500000, // Ropsten has a lower block limit than mainnet
-      confirmations: 2, // # of confs to wait between deployments. (default: 0)
-      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
-      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
-    },
-  },
+  // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.7",
+      version: "0.8.7", // Fetch exact version from solc-bin (default: truffle's version)
     },
   },
   contracts_directory: "./contracts/",
   contracts_build_directory: "./build/",
+  plugins: ["truffle-plugin-verify"],
+  api_keys: {
+    etherscan: ETHERSCAN_API_KEY,
+  },
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*", // Any network (default: none)
+    },
+    ropsten: {
+      provider: () =>
+        new HDWalletProvider(
+          MNEMONIC,
+          `https://ropsten.infura.io/v3/${INFURA_PROJECT_ID}`
+        ),
+      network_id: 3,
+      gas: 5500000,
+      gasPrice: 10e9,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true,
+    },
+    rinkeby: {
+      provider: () =>
+        new HDWalletProvider(
+          MNEMONIC,
+          `https://rinkeby.infura.io/v3/${INFURA_PROJECT_ID}`
+        ),
+      network_id: 4,
+      gasPrice: 10e9,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true,
+    },
+    goerli: {
+      provider: () =>
+        new HDWalletProvider(
+          MNEMONIC,
+          `https://goerli.infura.io/v3/${INFURA_PROJECT_ID}`
+        ),
+      network_id: 5,
+      gasPrice: 10e9,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true,
+    },
+    kovan: {
+      provider: () =>
+        new HDWalletProvider(
+          MNEMONIC,
+          `https://kovan.infura.io/v3/${INFURA_PROJECT_ID}`
+        ),
+      network_id: 42,
+      gasPrice: 10e9,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true,
+    },
+  },
+
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    // timeout: 100000
+  },
 };
